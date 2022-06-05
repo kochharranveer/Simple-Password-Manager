8ffa34b7a72576e7bbeead342c523c451831ff73
kochharranveer <kochharranveer@gmail.com>
2022-06-05 08:52:36 +0530

    main

---
+++ passwordManager.py
@@ -0,0 +1,58 @@
+def view () :
+
+    file = open ("passwordTextFile.txt","r")
+    
+    print ()
+    print ("Your Passwords : ")
+    print ()
+    print ( file.read() )
+    print()
+
+    file.close ()
+
+    SelectMode()
+
+def add () :
+
+    file = open ("passwordTextFile.txt","a")
+
+    EmailUsername = input ("Enter the email or username : ")
+    Password = input ("Enter the password : ")
+
+    file.write (EmailUsername)
+    file.write (" ")
+    file.write (Password)
+    file.write (" ")
+
+    file.close ()
+
+    print ()
+    print ("Added !")
+
+    SelectMode ()
+
+def invalidCode () :
+
+    print ("You entered an invalid code as a mode !")
+    
+    SelectMode ()
+
+def SelectMode () :
+
+    print ()
+    Mode = input ("Enter view if you want to view your passwords,\nadd if you want to add a new password\nand exit if you want to quit the program : ")
+    print ()
+
+    if Mode == "view" :
+        view()
+
+    elif Mode == "add" :
+        add()
+
+    elif Mode == "exit" :
+        quit()
+    
+    else :
+        invalidCode()
+
+SelectMode ()
