d475737bf3acc0375e461697f85030300843ba64
kochharranveer <kochharranveer@gmail.com>
2022-06-06 06:31:36 +0530

    main

--- passwordManager.py
+++ passwordManager.py
@@ -1,18 +1,29 @@
-def view () :
+addCount = 0
 
-    file = open ("passwordTextFile.txt","r")
+def view (addCountLocal) :
+
+    if addCountLocal >= 1 :
+
+        file = open ("passwordTextFile.txt","r")
     
-    print ()
-    print ("Your Passwords : ")
-    print ()
-    print ( file.read() )
-    print()
+        print ()
+        print ("Your Passwords : ")
+        print ()
+        print ( file.read() )
+        print()
 
-    file.close ()
+        file.close ()
 
-    SelectMode()
+        SelectMode(addCountLocal)
+    
+    else :
 
-def add () :
+        print()
+        print ("You haven't added anything yet !")
+        print()
+        SelectMode(addCountLocal)
+
+def add (addCountLocal) :
 
     file = open ("passwordTextFile.txt","a")
 
@@ -22,37 +33,38 @@ def add () :
     file.write (EmailUsername)
     file.write (" ")
     file.write (Password)
-    file.write (" ")
+    file.write ("\n")
 
     file.close ()
 
     print ()
     print ("Added !")
 
-    SelectMode ()
+    SelectMode (addCountLocal)
 
-def invalidCode () :
+def invalidCode (addCountLocal) :
 
     print ("You entered an invalid code as a mode !")
     
-    SelectMode ()
+    SelectMode (addCountLocal)
 
-def SelectMode () :
+def SelectMode (addCountLocal) :
 
     print ()
     Mode = input ("Enter view if you want to view your passwords,\nadd if you want to add a new password\nand exit if you want to quit the program : ")
     print ()
 
     if Mode == "view" :
-        view()
+        view(addCountLocal)
 
-    elif Mode == "add" :
-        add()
+    if Mode == "add" :
+        addCountLocal += 1
+        add(addCountLocal)
 
     elif Mode == "exit" :
         quit()
     
     else :
-        invalidCode()
+        invalidCode(addCountLocal)
 
-SelectMode ()
+SelectMode (addCount)
